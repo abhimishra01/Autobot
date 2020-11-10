0187b097137e69be29ce88d4f295e1905cf54410
root <root@localhost.localdomain>
2020-11-10 13:06:30 +0530

    v1.0

--- Autobot.py
+++ Autobot.py
@@ -1,12 +1,4 @@
 import os 
-os.system('clear')
-os.system('tput setaf  3')
-print('Please wait while we are resolving dependencies....!')
-os.system("rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm")
-os.system("dnf install figlet -y")
-os.system("pip3 install yattag")
-os.system('clear')
-os.system('tput setaf 3')
 while True:
     os.system('tput setaf  1')
     os.system('figlet  Autobot: The Automation Tool')
